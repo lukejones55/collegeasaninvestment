80051b3193b6da2ba9fa00033709de8c2b232b87
Tyler Mathern <math3rn@gmail.com>
2020-04-16 13:41:50 -0600

    Updates

---
+++ precache-manifest.aa3a14d9f7d1ecf122cc6abe81e767fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30f8ada6659eb7b981b861747bc10bae",
+    "url": "/collegeasaninvestment/index.html"
+  },
+  {
+    "revision": "a04139b9c85437551bc0",
+    "url": "/collegeasaninvestment/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5b4db5139f21b16f4df6",
+    "url": "/collegeasaninvestment/static/js/2.f123ac7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/collegeasaninvestment/static/js/2.f123ac7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a04139b9c85437551bc0",
+    "url": "/collegeasaninvestment/static/js/main.d3d3ba22.chunk.js"
+  },
+  {
+    "revision": "2b7677edc62df7c2bfeb",
+    "url": "/collegeasaninvestment/static/js/runtime-main.40e830bf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/collegeasaninvestment/static/media/logo.5d5d9eef.svg"
+  }
+]);
